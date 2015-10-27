05ec679eca1db28a7bc24d106032d3166e0c554b
Chidiebere Nnadi <chidiebere.nnadi@gmail.com>
2015-10-27 01:45:07 +0100

    Restructured folders and added in Gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp'),
+  rename = require('gulp-rename'),
+  sass = require('gulp-sass');
+
+gulp.task('sass', function() {
+  return gulp.src('src/css/flexgrid.scss')
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('build', ['sass']);
