7d15d141767e189acd07a17977ebc084f9d50476
Chidiebere Nnadi <chidiebere.nnadi@gmail.com>
2015-10-27 01:48:50 +0100

    Build of the latest stable release

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,8 @@ var gulp = require('gulp'),
 
 gulp.task('sass', function() {
   return gulp.src('src/css/flexgrid.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('dist/css'))
     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
     .pipe(rename({ suffix: '.min' }))
     .pipe(gulp.dest('dist/css'));
